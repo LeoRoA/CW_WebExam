3c342d835ba9ee8538c2583f01a03f53324e2abf
LeoRoA <romanleontev@gmail.com>
2023-01-19 18:49:34 +0700

    Initial commit

---
+++ src/main/java/com/example/cw_webexam/CwWebExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.cw_webexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwWebExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CwWebExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cw_webexam/CwWebExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cw_webexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwWebExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
